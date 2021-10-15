9e5b363580cac12106749dbf1dee391a89490059
PauloKeller <paulo.vinicius.keller@gmail.com>
2021-10-15 20:56:25 -0300

    first commit

---
+++ Artificial-Intelligence-A-Z/Self_Driving_Car/ai.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 15 20:27:43 2021
+
+@author: paulo
+"""
+
+
+import numpy as np
+import random
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.autograd as autograd
+from torch.autograd import Variable
+
+# Creating the architecture of the Neural Network
+
+class Network(nn.Module):
+    
+    def __init__(self, input_size, nb_action):
+        super(Network, self).__init__()
+        self.input_size = input_size
+        self.nb_action = nb_action
+        self.fc1 = nn.Linear(input_size, 30)
+        self.fc2 = nn.Linear(30, nb_action)
+        
