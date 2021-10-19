c368e067ec9c02e18acc610705df4dd329a4c4cb
PauloKeller <paulo.vinicius.keller@gmail.com>
2021-10-19 19:30:28 -0300

    learn function done

--- Artificial-Intelligence-A-Z/Self_Driving_Car/ai.py
+++ Artificial-Intelligence-A-Z/Self_Driving_Car/ai.py
@@ -49,4 +49,35 @@ class ReplayMemory(object):
     def sample(self, batch_size):
         samples = zip(*random.sample(self.memory, batch_size))
         return map(lambda x: Variable(torch.cat(x, 0)), samples)
+    
+# Implementing Deep Q Learning
+
+class Dqn():
+    
+    def __init__(self, input_size, nb_action, gamma):
+        self.gamma = gamma
+        self.reward_window = []
+        self.model = Network(input_size, nb_action)
+        self.memory = ReplayMemory(100000)
+        self.optimizer = optim.Adam(self.model.parameters(), lr = 0.001)
+        self.last_state = torch.Tensor(input_size).unsqueeze(0)
+        self.last_action = 0
+        self.last_reward = 0
+        
+    def select_action(self, state):
+        probs = F.softmax(self.model(Variable(state, volatile = True))*7)
+        action = probs.multinomial()
+        return action.data[0,0]
+    
+    def learn(self, batch_state, batch_next_state, batch_reward, batch_action):
+        outputs = self.model(batch_state).gather(1, batch_action.unsqueeze(1)).squeeze(1)
+        next_outputs = self.model(batch_next_state).detach().max(1)[0]
+        target = self.gamma * next_outputs + batch_reward
+        td_loss = F.smooth_l1_loss(outputs, target)
+        self.optimizer.zero_grad()
+        td_loss.backward(retain_variables = True)
+        self.optimizer.step()
+        
+        
+        
         
