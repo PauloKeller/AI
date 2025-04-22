f6a17a001e7326c1b18cdde777faa0de1a8bc019
Paulo Keller <paulo.vinicius.keller@gmail.com>
2025-04-22 20:32:31 -0300

    machine learning

---
+++ MuscleFitness/MuscleFitness/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MuscleFitness
+//
+//  Created by Paulo Keller on 15/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MuscleFitness/MuscleFitness/MuscleFitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuscleFitnessApp.swift
+//  MuscleFitness
+//
+//  Created by Paulo Keller on 15/03/25.
+//
+
+import SwiftUI
+
+@main
+struct MuscleFitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
